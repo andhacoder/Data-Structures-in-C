79ab85cb4867903b10cae1e786e901f50df81d67
Mridul Singh <msmridulsingh@gmail.com>
2019-04-21 15:01:00 +0530

    Create Reverse_linkedlist.c
    Iterative function to reverse a linked list

---
+++ Reverse_linkedlist.c
@@ -0,0 +1,65 @@
+//Deleting node at the nth position
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node* head;
+
+void insert(int data)
+{
+    struct node* temp = (node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+    if(head == NULL)
+    {
+        head = temp;
+        return;
+    }
+    struct node* temp2 = head;
+    while(temp2->next != NULL)
+    {
+        temp2 = temp2->next;
+    }
+    temp2->next = temp;
+}
+
+void reverse()
+{
+    struct node *current,*prev,*next;
+    current = head;
+    prev = NULL;
+    while( current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
+
+void print()
+{
+    struct node* temp = head;
+    while(temp != NULL)
+    {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    insert(2);
+    insert(4);
+    insert(6);
+    insert(5);
+    print();
+    reverse();
+    print();
+}
