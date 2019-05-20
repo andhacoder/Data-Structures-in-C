afcf8cdc77276c805e6c28d499c1dfd22f0cfaf0
Mridul Singh <msmridulsingh@gmail.com>
2019-05-20 13:56:49 +0530

    Create reverse_recursion.c

---
+++ reverse_recursion.c
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* head;
+
+
+void reverse(struct node* p){
+    if(p->next == NULL){
+        head = p;
+        return;
+    }
+    reverse(p->next);
+    struct node* q = p->next;
+    q->next = p;
+    p->next = NULL;
+}
+
+
